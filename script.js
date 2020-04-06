93d6976f76fcdf200c6581975b008c773fb5c47a
codershona <falgunimst95@gmail.com>
2020-04-06 18:37:07 +0200

    working on search buton

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // function
-let appId = "478cc3c9e7cd7db831d6f004db3d70f7";
+let appId = '478cc3c9e7cd7db831d6f004db3d70f7';
 let units = 'imperial';
-let searchMethod;
+let searchMethod = 'zip';
 
 function searchWeather(searchTerm) {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}`)
+  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+    return result.json();
+  }).then(result => {
+    // IntersectionObserver();
+    init(result);
+  })
 
+}
+
+function init(resultFromServer) {
+      console.log(resultFromServer);
 }
