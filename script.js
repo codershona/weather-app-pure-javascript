52b1908447ce509f98a5b5cba67b08e09e8e1fd0
codershona <falgunimst95@gmail.com>
2020-04-06 18:30:05 +0200

    working on js and using backtacks for url

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // function
+let appId = "478cc3c9e7cd7db831d6f004db3d70f7";
+let units = 'imperial';
+let searchMethod;
 
+function searchWeather(searchTerm) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}`)
 
-// weather api key = 478cc3c9e7cd7db831d6f004db3d70f7
+}
